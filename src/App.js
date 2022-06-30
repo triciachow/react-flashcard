203ea729e3aea9e1182f2d6fdfe803b69ca4b3be
triciachow0212 <suyeechow0212@gmail.com>
2022-07-01 01:40:09 +0800

    Create form to customise questions

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import FlashcardList from "./FlashcardList";
 import "./app.css";
 import axios from "axios";
 
 export default function App() {
   const url = "https://opentdb.com/api.php?amount=10";
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARD);
+  const categoryUrl = "https://opentdb.com/api_category.php";
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
-    axios.get(url).then(res => {
-      setFlashcards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer);
-          const options = [
-            ...questionItem.incorrect_answers.map(ans => decodeString(ans)),
-            answer,
-          ];
-
-          return {
-            id: `${index}-${Date.now()}`, // To ensure a unique ID
-            question: decodeString(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random() - 0.5),
-          };
-        })
-      );
+    axios.get(categoryUrl).then(res => {
+      setCategories(res.data.trivia_categories);
     });
   }, []);
+
+  useEffect(() => {}, []);
 
   // Translate HTML character coding to normal string
   function decodeString(str) {
@@ -35,26 +26,69 @@ export default function App() {
     return textArea.value;
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get(url, {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value,
+        },
+      })
+      .then(res => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map(ans => decodeString(ans)),
+              answer,
+            ];
+
+            return {
+              id: `${index}-${Date.now()}`, // To ensure a unique ID
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
+  }
+
   return (
     <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category, index) => {
+              return (
+                <option value={category.id} key={index}>
+                  {category.name}
+                </option>
+              );
+            })}
+            <option></option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
       <div className="container">
         <FlashcardList flashcards={flashcards} />
       </div>
     </>
   );
 }
-
-const SAMPLE_FLASHCARD = [
-  {
-    id: 1,
-    question: "What is 2 + 2??",
-    answer: "4",
-    options: ["2", "3", "4", "5"],
-  },
-  {
-    id: 2,
-    question: "What is 3 + 2?",
-    answer: "5",
-    options: ["2", "3", "4", "5"],
-  },
-];
