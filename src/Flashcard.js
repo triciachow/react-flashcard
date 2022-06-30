0999e7476fb51db6caf41e9a789860a741f7d528
triciachow0212 <suyeechow0212@gmail.com>
2022-06-30 17:46:02 +0800

    Init new file

---
+++ src/Flashcard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Flashcard() {
+  return <div>Flashcard</div>;
+}
+
+export default Flashcard;
---
+++ src/FlashcardList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FlashcardList() {
+  return <div>FlashcardList</div>;
+}
+
+export default FlashcardList;
